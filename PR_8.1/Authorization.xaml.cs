c1d57cf559056c7352239977e6a32d368887a3e9
D1sTc <143994071+D1sTc@users.noreply.github.com>
2024-06-25 13:48:18 +0300

    Add files via upload

---
+++ PR_8.1/Authorization.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PR_8._1
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Autorization_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            MakingAnOrder winm = new MakingAnOrder();
+            winm.Show();
+
+            //// для администратора:
+
+            //Close();
+            //MainMenu mm = new MainMenu();
+            //mm.Show();
+
+        //if (loginBox.Text.Length > 0) // проверяем введён ли логин     
+        //{
+        //if (passwordBox.Password.Length > 0) // проверяем введён ли пароль         
+        //{             // ищем в базе данных пользователя с такими данными         
+        //DataTable dt_user = MainWindow.Select("SELECT * FROM [dbo].[users] WHERE [login] = '" + loginBox.Text + "' AND [password] = '" + passwordBox.Password + "'");
+        //if (dt_user.Rows.Count > 0) // если такая запись существует       
+        //MessageBox.Show("Пользователь авторизовался"); // говорим, что авторизовался         
+        //else MessageBox.Show("Пользователя не найден"); // выводим ошибку  
+        //}
+        //else MessageBox.Show("Введите пароль"); // выводим ошибку    
+        //}
+        //else MessageBox.Show("Введите логин"); // выводим ошибку 
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ PR_8.1/MakingAnOrder.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PR_8._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MakingAnOrder.xaml
+    /// </summary>
+    public partial class MakingAnOrder : Window
+    {
+        public MakingAnOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MakeOrder_Click(object sender, RoutedEventArgs e)
+        {
+            // Отчёт о выбранных данных идёт в папку "Заказы"
+
+            MessageBox.Show("Заказ на исполнение альбома отправлен. Ожидайте сообщения от администрации.", "Оформление заказа", MessageBoxButton.OK, MessageBoxImage.Question); // Question - знак с вопросом, необходимо подделать личный знак.
+        }
+    }
+}
